d97bcae44377f4261d682938a7f9188a3161fe9f
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-05-13 03:17:14 +0000

    feat: implement service layer for all questions, repsonses, catecories etc...

--- backend/app/services/question_service.ts
+++ backend/app/services/question_service.ts
@@ -1,3 +1,29 @@
+import Question from "#models/question"
+
 export class QuestionService {
-  // Your code here
+
+    async getAllQuestions() {
+        return Question.all()
+    }
+
+    async getQuestionById(id: number) {
+        return Question.query().where('id', id).first()
+    }
+
+    async createQuestion(data: Partial<Question>) {
+        return Question.create(data)
+    }
+
+    async updateQuestion(id: number, data: Partial<Question>) {
+        const question = await Question.findOrFail(id)
+        question.merge(data)
+        await question.save()
+        return question
+    }
+
+    async deleteQuestion(id: number) {
+        const question = await Question.findOrFail(id)
+        await question.delete()
+        return question
+    }
 }
--- backend/app/services/response_service.ts
+++ backend/app/services/response_service.ts
@@ -1,3 +1,29 @@
+import Response from "#models/response"
+
 export class ResponseService {
-  // Your code here
+
+    async getAllResponses() {
+        return Response.all()
+    }
+
+    async getResponseById(id: number) {
+        return Response.query().where('id', id).first()
+    }
+
+    async createResponse(data: Partial<Response>) {
+        return Response.create(data)
+    }
+
+    async updateResponse(id: number, data: Partial<Response>) {
+        const response = await Response.findOrFail(id)
+        response.merge(data)
+        await response.save()
+        return response
+    }
+
+    async deleteResponse(id: number) {
+        const response = await Response.findOrFail(id)
+        await response.delete()
+        return response
+    }
 }
