4ce2259d7ca6316c2a0d029321b041109b11b05f
yaya <ydabo1876@gmail.com>
2026-06-08 23:19:27 +0000

    configuring authentification and register,login done !!!!!!!!!!!!!!!!!!

---
+++ Backend/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+// Public routes
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+
