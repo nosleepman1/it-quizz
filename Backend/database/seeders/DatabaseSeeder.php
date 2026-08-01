56cd6b0f6fefa4cda8dbea7837afd32b9b0af97d
yaya <ydabo1876@gmail.com>
2026-08-01 22:06:56 +0000

    feat(quiz): mise en place du declenchement des quiz (mode rapide 20 questions et mode sur-mesure)

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -2,14 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Badge;
-use App\Models\Choice;
-use App\Models\Question;
-use App\Models\Subcategory;
-use App\Models\Topic;
-use App\Models\User;
-use App\Models\Theme;
-use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -22,16 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User
-        // User::factory()->create([
-        //     'username' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        // Core seeders
         $this->call([
             UserSeeder::class,
-            QuizDataSeeder::class,
+            ThemeSeeder::class,
+            CategorySeeder::class,
+            SubcategorySeeder::class,
+            TopicSeeder::class,
+            QuestionSeeder::class,
+            ChoiceSeeder::class,
             BadgeSeeder::class,
         ]);
     }
